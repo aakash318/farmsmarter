4a62728a760dc8faa854db4ba9113522ae94e207
Aakash Pathak <streetgamer1335@gmail.com>
2026-05-25 15:00:34 +0530

    1thime

--- src/App.jsx
+++ src/App.jsx
@@ -5,53 +5,53 @@ import Header from './myComponent/Header'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Footer from './myComponent/Footer'
 import HeroSection from './myComponent/HeroSection'
-
 import Farmer from './myComponent/Farmer'
 import "./myComponent/myStyle/App.css"
-import Farmerinfo from './myComponent/Farmerinfo'
+import FarmerInfo from './myComponent/FarmerInfo'
 import FarmerRegister from './myComponent/FarmerRegister'
 import FarmerLogin from './myComponent/FarmerLogin'
-import FarmerProfile from './myComponent/farmerProfile'
+import FarmerProfile from './myComponent/FarmerProfile'
 import AddProduct from './myComponent/AddProduct'
 import ProductDetails from './myComponent/ProductDetails'
 import Cart from './myComponent/Cart'
-import Checkout from './myComponent/CheckOut'
-import FarmerInfo from './myComponent/Farmerinfo'
+import Checkout from './myComponent/Checkout'
 import axios from 'axios'
 import OrderSuccess from './myComponent/OrderSuccess'
 
-
-
-
 function App() {
+
    const [count, setCount] = useState(0);
    const [bill, setBill] = useState(0);
    const [totalBill, setTotalBill] = useState(0);
+
    useEffect(() => {
 
       const clearCart = () => {
 
-         axios.delete("http://localhost:5000/cart/clear/1")
-            .catch(err => console.log(err));
+         axios.delete(
+            `${import.meta.env.VITE_API_URL}/cart/clear/1`
+         )
+         .catch(err => console.log(err));
 
       };
 
-      window.addEventListener("beforeunload", clearCart);
+      window.addEventListener(
+         "beforeunload",
+         clearCart
+      );
 
       return () => {
+
          window.removeEventListener(
             "beforeunload",
             clearCart
          );
+
       };
 
    }, []);
 
-
-
    return (
-
-
 
       <BrowserRouter>
 
@@ -59,30 +59,94 @@ function App() {
 
          <Routes>
 
-            <Route path='/' element={<HeroSection />} />
-            <Route path='/farmer' element={<Farmer count={count} setCount={setCount} bill={bill} setBill={setBill} />} />
-            <Route path='farmerinfo' element={<FarmerInfo />} />
+            <Route
+               path='/'
+               element={<HeroSection />}
+            />
 
-            <Route path='/farmer/farmerRegister' element={<FarmerRegister />} />
-            <Route path='/farmer/farmerLogin' element={<FarmerLogin />} />
-            <Route path='/farmer/profile' element={<FarmerProfile />} />
-            <Route path='/profile/addProduct' element={<AddProduct />} />
-            <Route path='/product/:id' element={<ProductDetails count={count} setCount={setCount} bill={bill} setBill={setBill} />} />
-            <Route path='/cart' element={<Cart count={count} setCount={setCount} bill={bill} setBill={setBill} totalBill={totalBill} setTotalBill={setTotalBill} />} />
-            <Route path='/checkout' element={<Checkout totalBill={totalBill} setTotalBill={setTotalBill} />} />
-            <Route path="/order-success" element={<OrderSuccess />} />
+            <Route
+               path='/farmer'
+               element={
+                  <Farmer
+                     count={count}
+                     setCount={setCount}
+                     bill={bill}
+                     setBill={setBill}
+                  />
+               }
+            />
+
+            <Route
+               path='/farmerinfo'
+               element={<FarmerInfo />}
+            />
+
+            <Route
+               path='/farmer/farmerRegister'
+               element={<FarmerRegister />}
+            />
+
+            <Route
+               path='/farmer/farmerLogin'
+               element={<FarmerLogin />}
+            />
+
+            <Route
+               path='/farmer/profile'
+               element={<FarmerProfile />}
+            />
+
+            <Route
+               path='/profile/addProduct'
+               element={<AddProduct />}
+            />
+
+            <Route
+               path='/product/:id'
+               element={
+                  <ProductDetails
+                     count={count}
+                     setCount={setCount}
+                     bill={bill}
+                     setBill={setBill}
+                  />
+               }
+            />
+
+            <Route
+               path='/cart'
+               element={
+                  <Cart
+                     count={count}
+                     setCount={setCount}
+                     bill={bill}
+                     setBill={setBill}
+                     totalBill={totalBill}
+                     setTotalBill={setTotalBill}
+                  />
+               }
+            />
+
+            <Route
+               path='/checkout'
+               element={
+                  <Checkout
+                     totalBill={totalBill}
+                     setTotalBill={setTotalBill}
+                  />
+               }
+            />
+
+            <Route
+               path="/order-success"
+               element={<OrderSuccess />}
+            />
+
          </Routes>
-
 
          <Footer />
 
-
       </BrowserRouter>
-
-
-
-
-
 
    )
 
